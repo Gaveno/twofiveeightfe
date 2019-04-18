1d3fd9b3754534da5454eca04d01f2c1d3d7d23c
Gavin <gavin.monkeystudios@gmail.com>
2019-04-17 20:38:54 -0600

    Added banner page

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
-import MovieHeader from './components/movieheader';
-import MovieList from './components/movielist';
-import Movie from './components/movie';
 import {HashRouter,Route} from 'react-router-dom';
+import banner from './Banner2_1.png';
 
 //add routing configuration
 
 class App extends Component {
-  state = {movieId: null};
+    state = {movieId: null};
 
-  handleOnTitleChange = (e) => {
-    this.setState({
-      movieId: e
-    });
-  }
-  render() {
-    return (
-        <div className="App">
-          <HashRouter>
-            <div>
-              <MovieHeader movieId={this.state.movieId}/>
-              <Route exact path="/" render={()=><MovieList onTitleChange={this.handleOnTitleChange} />}/>
-              <Route path="/movielist" render={()=><MovieList onTitleChange={this.handleOnTitleChange} />}/>
-              <Route path="/movie" render={()=><Movie onTitleChange={this.handleOnTitleChange}/>}/>
+    handleOnTitleChange = (e) => {
+        this.setState({
+            movieId: e
+        });
+    }
+    render() {
+        return (
+            <div className="App">
+                <HashRouter>
+                    <div>
+                        <img src={banner} alt="It's coming... 2FIVEEIGHT" />
+                    </div>
+                </HashRouter>
             </div>
-          </HashRouter>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
