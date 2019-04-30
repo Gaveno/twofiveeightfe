da5bb9ec41de1ae31ccc47bfe50e5b734ffd70f7
Tammy Husnetdinova <tamhus31@gmail.com>
2019-04-30 16:07:29 -0600

    userFeed header implemented (profile picture, follow button, follower, and following counts)

--- src/components/renderuser.js
+++ src/components/renderuser.js
@@ -3,6 +3,9 @@ import defaultProfilePhoto from "../images/defaultProfilePhoto.png";
 import profilePhotoCrop from "../images/profilePhotoCrop.png";
 import React from 'react';
 import {Divider} from './divider';
+import dummyimage from "../images/dummyimage.jpg";
+import btnComment from "../images/btnComment.png";
+import btnRepost from "../images/btnRepost.png";
 
 function arrayBufferToBase64(buffer) {
     let binary = '';
@@ -13,27 +16,32 @@ function arrayBufferToBase64(buffer) {
 
 export const RenderUser = ({user}) => {
     return (
-        <Grid>
-            <div>
-                <Row className="user-feed">
-                    <Col>
-                        <img className="user-feed-profile-image"
-                         src={user.imgProfile ? user.imgProfile : defaultProfilePhoto}/>
-                        <img className="user-feed-profile-crop"
-                         src={profilePhotoCrop}/>
-                    </Col>
-                    <Col xs={20}>
-                        <b className="user-feed-username">{user.username ? user.username : "username"}</b>
-                    </Col>
-                    <Col xs={20}>
-                        <Button className="user-feed-follow-button" type="button">Follow</Button>
-                    </Col>
-                    <Col xs={20}>
-                        <p className="user-feed-follower-count">Followers:{user.followerCount ? user.followerCount : "0"}</p>
-                    </Col>
+        <Row>
+            <Grid className="user-feed">
+                <Row className="user-feed-row">
+                        <Col xs={5} className="user-feed-left-column">
+                            <img className="user-feed-profile-image"
+                             src={user.imgProfile ? user.imgProfile : defaultProfilePhoto}/>
+                            <img className="user-feed-profile-crop"
+                             src={profilePhotoCrop}/>
+                        </Col>
+                        <Col xs={5} className="user-feed-right-column">
+                            <Row className="user-feed-row-1">
+                                <b className="user-feed-username">{user.username ? user.username : "username"}</b>
+                            </Row>
+                            <Row className="user-feed-row-1">
+                                <Button className="user-feed-follow-button" type="button">Follow</Button>
+                            </Row>
+                            <Row className="user-feed-row-1">
+                                <p className="user-feed-follower-count">{user.followerCount ? user.followerCount : "0"} Followers</p>
+                            </Row>
+                            <Row className="user-feed-row-1">
+                                <p className="user-feed-following-count">Following {user.following ? user.following : "0"}</p>
+                            </Row>
+                            </Col>
                 </Row>
-                <Divider/>
-            </div>
-        </Grid>
+            </Grid>
+            <Divider/>
+        </Row>
     )
 };
