b3092b0169a075797b2172dfa3d7bfc1bbdffe53
Gavin <gavin.monkeystudios@gmail.com>
2019-05-05 14:53:36 -0600

    Updated RenderFollowing

--- src/components/renderfollowers.js
+++ src/components/renderfollowers.js
@@ -1,20 +1,27 @@
 import defaultProfilePhoto from "../images/defaultProfilePhoto.png";
 import React from 'react';
 import Username from "./username";
-import {Divider} from "./divider";
 import {arrayBufferToBase64} from "../actions/helpers";
+import profilePhotoCrop from "../images/profilePhotoCrop.png";
 
 
 export const RenderFollowers = ({users}) => {
     return users.map((user, i) =>
         <div key={i} className="followers">
-            <img className="follower-list"
-                 src={(user.imgProfile && user.imgProfile.data) ?
-                     `data:image/jpeg;base64,${arrayBufferToBase64(user.imgProfile.data.data)}` :
-                     defaultProfilePhoto}
-                 alt="user profile"/>
-            <Username username={user.username} />
-            <Divider/>
+            <div className="followers-photo-container">
+                <img className="after"
+                    src={(user.imgProfile && user.imgProfile.data) ?
+                         `data:image/jpeg;base64,${arrayBufferToBase64(user.imgProfile.data.data)}` :
+                         defaultProfilePhoto}
+                     alt="user profile"/>
+                <img className="after"
+                     src={profilePhotoCrop}
+                     alt="crop user profile"/>
+
+            </div>
+            <div className="followers-username">
+                <Username username={user.username} />
+            </div>
         </div>
     )
 
