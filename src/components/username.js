e722d054593f39765c920c86d77fa377ea4582a2
Gavin <gavin.monkeystudios@gmail.com>
2019-05-21 09:02:15 -0600

    Fixed Clicking User in Userfeed
    - When clicking on a username in the userfeed it now correctly reloads the page.
    - When changing user, the feed scroll now resets to the top.

--- src/components/username.js
+++ src/components/username.js
@@ -1,16 +1,25 @@
 import React, {Component} from 'react';
 import {LinkContainer} from 'react-router-bootstrap';
+import {getPath} from "../actions/helpers";
 
 export default class Username extends Component {
+    onClicked() {
+        if (getPath() === "userfeed") {
+            localStorage.setItem('userScroll', "0");
+            window.location.reload();
+        }
+    }
     render() {
         if (!this.props.username)
             return "@usernotfound";
         else {
             const username = this.props.username;
             return (
-                <LinkContainer to={"/userfeed/" + username.split(" ")[0]}>
-                    <b>@{username}</b>
-                </LinkContainer>
+                <samp className="username" onClick={this.onClicked}>
+                    <LinkContainer to={"/userfeed/" + username.split(" ")[0]}>
+                        <b>@{username}</b>
+                    </LinkContainer>
+                </samp>
             )
         }
     }
