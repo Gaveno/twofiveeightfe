5ee8e5ab105621a537149ba55b179a261311c2d7
Gavin <gavin.monkeystudios@gmail.com>
2020-01-05 11:04:15 -0700

    Updated creator info.

--- src/components/authentication/authfooter.js
+++ src/components/authentication/authfooter.js
@@ -3,8 +3,8 @@ import React from 'react';
 export const AuthFooter = () => {
     return (
         <div className="auth-footer">
-            Created by Gavin Atkin, Tamila Husnetdinova, and Erik Nelson for WebAPI at the University of Colorado Denver
-            spring 2019.
+            Created by Gavin Atkin. Contributors: Anthony Dupont, Tuan-Ahn Le, Tamila Husnetdinova, and Erik Nelson.
+            Originally created for WebAPI at the University of Colorado Denver spring 2019.
         </div>
     );
 };
